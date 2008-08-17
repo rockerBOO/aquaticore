eb5ab72dc418fffc0f7f4fdf5885b8e10a7fac9e
rockerboo <rockerboo@5eaf30e1-ec0c-44b8-8f50-1827c21fd50b>
2008-08-17 18:58:11 +0000

    Adding fish, plant
    git-svn-id: https://rockerboo.svn.beanstalkapp.com/aquaticore@3 5eaf30e1-ec0c-44b8-8f50-1827c21fd50b

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'aquaticore'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'firefly12!'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE    = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME      = 'aquaticore'             # Or path to database file if using sqlite3.
+DATABASE_USER      = 'root'             # Not used with sqlite3.
+DATABASE_PASSWORD  = 'firefly12!'         # Not used with sqlite3.
+DATABASE_HOST      = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT      = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
