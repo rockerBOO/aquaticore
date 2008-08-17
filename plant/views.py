32fab916bb977c5a6678b34888cdc44bdb5d2e5c
rockerboo <rockerboo@5eaf30e1-ec0c-44b8-8f50-1827c21fd50b>
2008-08-17 21:22:44 +0000

    Adding contact, privacy, tos
    git-svn-id: https://rockerboo.svn.beanstalkapp.com/aquaticore@4 5eaf30e1-ec0c-44b8-8f50-1827c21fd50b

--- plant/views.py
+++ plant/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+    return render_to_response('plant/index.html')
